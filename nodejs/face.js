669432c42296420a8ac26efad0467360548e0ad8
fay <869770665@qq.com>
2020-06-23 18:42:34 +0800

    upd

--- nodejs/face.js
+++ nodejs/face.js
@@ -2,6 +2,27 @@ const request = require('request');
 const fs = require('fs');
 const path = require('path');
 
+const errors = {
+  'IMAGE_ERROR_UNSUPPORTED_FORMAT: image_base64_1': '正脸的图像无法解析',
+  'IMAGE_ERROR_UNSUPPORTED_FORMAT: image_base64_2': '左脸的图像无法解析',
+  'IMAGE_ERROR_UNSUPPORTED_FORMAT: image_base64_3': '右脸的图像无法解析',
+  'INVALID_IMAGE_SIZE: image_base64_1': '正脸的图像尺寸不符合',
+  'INVALID_IMAGE_SIZE: image_base64_2': '左脸的图像尺寸不符合',
+  'INVALID_IMAGE_SIZE: image_base64_3': '右脸的图像尺寸不符合',
+  'INVALID_IMAGE_URL': '上传的图像文件无法解析',
+  'IMAGE_FILE_TOO_LARGE: image_base64_1': '正脸的图像文件太大，不能超过2M',
+  'IMAGE_FILE_TOO_LARGE: image_base64_2': '左脸的图像文件太大，不能超过2M',
+  'IMAGE_FILE_TOO_LARGE: image_base64_3': '右脸的图像文件太大，不能超过2M',
+  'IMAGE_DOWNLOAD_TIMEOUT': '上传图片超时',
+  'NO_FACE_FOUND: image_base64_1': '没有检测到正脸',
+  'NO_FACE_FOUND: image_base64_2': '没有检测到左脸',
+  'NO_FACE_FOUND: image_base64_3': '没有检测到右脸',
+  'MULTIPLE_FACE_FOUND: image_base64_1': '正脸的图像中出现多个人脸',
+  'MULTIPLE_FACE_FOUND: image_base64_2': '左脸的图像中出现多个人脸',
+  'MULTIPLE_FACE_FOUND: image_base64_3': '右脸的图像中出现多个人脸',
+  '': '人脸识别失败'
+};
+
 const save3DFiles = ({userId, face1, face2, face3}) => {
   const form = {
     "api_key": "oJLTb2ldY2YJK_HhtQR_l56ktD15dz6S",
@@ -27,15 +48,14 @@ const save3DFiles = ({userId, face1, face2, face3}) => {
           const obj_file = res.obj_file;
           const mtl_file = res.mtl_file;
           const texture_img = res.texture_img;
-          let objPath = path.join(basePath, 'face.obj');
-          let mtlPath = path.join(basePath, 'face.mtl');
-          let jpgPath = path.join(basePath, 'tex.jpg');
-          let face1Path = path.join(basePath, 'face1');
-          let face2Path = path.join(basePath, 'face2');
-          let face3Path = path.join(basePath, 'face3');
-          // console.log(filePath);
-          // fs.readdirSync(basePath);
+          let objPath = path.join(basePath, '/face.obj');
+          let mtlPath = path.join(basePath, '/face.mtl');
+          let jpgPath = path.join(basePath, '/tex.jpg');
+          let face1Path = path.join(basePath, '/face1');
+          let face2Path = path.join(basePath, '/face2');
+          let face3Path = path.join(basePath, '/face3');
           if(fs.existsSync(basePath)){
+            console.log(1);
             if(fs.existsSync(objPath)) fs.unlinkSync(objPath);
             if(fs.existsSync(mtlPath)) fs.unlinkSync(mtlPath);
             if(fs.existsSync(jpgPath)) fs.unlinkSync(jpgPath);
@@ -45,7 +65,6 @@ const save3DFiles = ({userId, face1, face2, face3}) => {
           }else{
             fs.mkdirSync(basePath);
           }
-          // const upStream = fs.createWriteStream(filePath);
           fs.writeFileSync(objPath, Buffer.from(obj_file, 'base64'));
           fs.writeFileSync(mtlPath, Buffer.from(mtl_file, 'base64'));
           fs.writeFileSync(jpgPath, Buffer.from(texture_img, 'base64'));
@@ -54,14 +73,16 @@ const save3DFiles = ({userId, face1, face2, face3}) => {
           fs.writeFileSync(face3Path, Buffer.from(face3, 'base64'));
           resolve({success: true, error});
         } catch (error) {
-          resolve({success: false, error});
+          resolve({success: false, error: errors['']});
         }
       }else{
-        resolve({success: false, error});
+        const err = JSON.parse(body);
+        resolve({success: false, error: errors[err.error_message]||errors['']});
       }
     })
   })
 }
+
 
 module.exports = {
   save3DFiles
