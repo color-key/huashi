4c342d6fdfe838362474f1e3f636080463e03b50
fay <869770665@qq.com>
2020-06-26 01:21:17 +0800

    upd

--- nodejs/auth.js
+++ nodejs/auth.js
@@ -4,7 +4,7 @@ const auth = async (ctx, callback) => {
   let authed = false;
   if(ctx.request.header.cookie){
     const cookieArr = ctx.request.header.cookie.split(';');
-    const hasUser = cookieArr.filter(item => item.indexOf('current-user=')===0);
+    const hasUser = cookieArr.filter(item => item.trim().indexOf('current-user=')===0);
     if(hasUser.length>0){
       const userEncode = hasUser[0].substr('current-user='.length);
       const user = JSON.parse(decodeURIComponent(userEncode));
