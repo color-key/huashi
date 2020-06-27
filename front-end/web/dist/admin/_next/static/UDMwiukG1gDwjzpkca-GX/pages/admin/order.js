8441ae62125a74d50c48f855caa290343f73aed9
fay <869770665@qq.com>
2020-06-28 03:29:24 +0800

    upd

--- front-end/web/dist/admin/_next/static/UDMwiukG1gDwjzpkca-GX/pages/admin/order.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{Batw:function(n,t,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/admin/order",function(){return w("pEMw")}])},pEMw:function(n,t,w){"use strict";w.r(t);var o=w("q1tI"),u=w.n(o),a=w("jmmT"),e=w("85fo"),i=u.a.createElement;t.default=function(){return i(a.a,null,i(e.a,null))}}},[["Batw",0,1,2,3,4,5,6]]]);
